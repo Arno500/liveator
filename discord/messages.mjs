e92a506fce5220f6dd365d8d53a5e46972fff5bc
Loadeksdi <nicolas.load@gmail.com>
2022-04-22 10:29:50 +0200

    Fix: stream thumbnail

--- discord/messages.mjs
+++ discord/messages.mjs
@@ -1,5 +1,5 @@
 const { MessageEmbed } = require('discord.js');
-const { default: client } = require('./index.mjs');
+const { client } = require('./index.mjs');
 
 export const streamStartEmbed = (streamInfo) => {
     const url = `https://twitch.tv/${streamInfo.username}`;
@@ -9,9 +9,9 @@ export const streamStartEmbed = (streamInfo) => {
         .setURL(url)
         .setAuthor({ name: streamInfo.username, iconURL: client.user.avatarURL, url })
         .setDescription(`**BendoooTV stream just started, we're playing ${streamInfo.game}! Come and join us here:**\n${url}`)
-        .setImage(streamInfo.thumbnail)
+        .setImage(streamInfo.thumbnail.replace('${width}', '500').replace('${height}', '500'))
         .setTimestamp(streamInfo.startedAt)
-        .setFooter({text: `${streamInfo.viewers} viewers`, iconURL: client.user.avatarURL})
+        .setFooter({text: `👁️ ${streamInfo.viewers} viewers`, iconURL: client.user.avatarURL})
 
     streamInfo.tags.forEach(tag => {
         streamStartMessage.addField(tag.name, tag.description)
